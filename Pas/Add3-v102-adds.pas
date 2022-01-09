cfe61e526582289210c74897a9dfa93cf359e6de
nevgoodyer <45184841+nevgoodyer@users.noreply.github.com>
2022-01-09 17:51:16 +1100

    v1.02 of all languages

--- Pas/Add3-v102-adds.pas
+++ Pas/Add3-v102-adds.pas
@@ -3,7 +3,7 @@ Uses crt; {standard Pascal library}
 Var
   number_1, number_2, number_3, total: Integer; {declarations}
 Begin
-  Writeln('v1.01 Add3. You will be asked for 3 numbers:'); {version}
+  Writeln('v1.02 Add3. You will be asked for 3 numbers:'); {version}
   Writeln('Enter first number'); {read user inputs}
   Readln(number_1);
   Writeln('Enter second number');
--- Py/Add3-v102-adds.py
+++ Py/Add3-v102-adds.py
@@ -1,5 +1,5 @@
 # add three numbers
-print("v1.01 Add3. You will be asked for 3 numbers:")   #version number
+print("v1.02 Add3. You will be asked for 3 numbers:")   #version number
 Number_1 = eval(input("Enter your first # : "))               #read user inputs
 Number_2 = eval(input("Enter your second # : "))
 Number_3 = eval(input("Enter your third # : "))
